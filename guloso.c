f8a7b33aec827c51de05669de97712051e54a6c7
Gabriel Dutra <nesk.frz@gmail.com>
2017-09-07 17:28:12 -0300

    Implementação do guloso inciciada

--- guloso.c
+++ guloso.c
@@ -58,16 +58,43 @@ int main(int argc, char *argv[]){
         printf("Ops, você não passou o arquivo de entrada!\nUso: %s arquivo-de-entrada\n", argv[0]);
         return 0;
     }
+
     FILE *arquivo = fopen(argv[1], "r");
     // Verifica caso não tenha sido possível abrir o arquivo
     if(!arquivo){
         printf("Não foi possível abrir o arquivo '%s'.\n", argv[1]);
         return 0;
     }
+
     Entrada entrada = carregaDados(arquivo);
 
     Item *solucao = NULL;
-    
+    Item *solucaoAtual = NULL;
+    int pesoTotal = 0;
+
+    // Implementação do algoritmo Guloso
+    while(1){
+        Item *maisValioso = NULL;
+        double custoBeneficioMV = 0;
+        
+        Item *item = entrada.itens;
+        while(item != NULL){ // Percorre todos os itens para encontrar o ótimo local
+            double custoBeneficio = (double) item->valor/item->peso;
+            if(custoBeneficio > custoBeneficioMV && pesoTOtal + item->peso < entrada.W){
+                maisValioso = item;
+                custoBeneficioMV = custoBeneficio;
+            }
+        }
+        if(maisValioso == NULL) break; // Caso não tenha maisValioso, sai do loop
+
+        if(solucao == NULL){
+            solucao = maisValioso;
+            solucaoAtual = maisValioso;
+        } else {
+            // TODO - S = S + maisValioso e C = C - maisValioso
+        }
+
+    }
     
     return 0;
 }
