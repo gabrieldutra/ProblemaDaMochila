c61d7431cab2a22bad6908042e3af46cadf4d236
aluno <nesk.frz@gmail.com>
2017-09-13 14:20:11 -0300

    POG para gerar soma de pesos específica facilmente

--- dataset/gerarData.sh
+++ dataset/gerarData.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-if [ "$#" -lt 6 ]  
+if [ "$#" -lt 7 ]  
 then
-    echo "Uso: $0 [Valor de N] [Valor de W] [Peso minimo] [Peso máximo] [Valor minimo] [Valor máximo]";
+    echo "Uso: $0 [Valor de N] [Valor de W] [Peso minimo] [Peso máximo] [Valor minimo] [Valor máximo] [Soma dos pesos]";
 else
-    n=$1;
-    w=$2;
-    pesoMinimo=$3;
-    pesoMaximo=$4;
-    valorMinimo=$5;
-    valorMaximo=$6;
-    arquivo="N"$n"-W"$w;
-    echo "$w" > $arquivo
-    echo "$n" >> $arquivo
-    t=0;
-    for (( i=1; i <= $n; i++))
+    # POG para gerar soma de pesos específica de maneira fácil
+    fim=0;
+    until [ $fim = 1 ]
     do
-        pesoItem=$(( ( RANDOM % ($pesoMaximo-$pesoMinimo+1) )  + $pesoMinimo ))
-        valorItem=$(( ( RANDOM % ($valorMaximo-$valorMinimo+1) )  + $valorMinimo ))
-        t=$((pesoItem+t));
-        echo "$pesoItem $valorItem" >> $arquivo
+        n=$1;
+        w=$2;
+        pesoMinimo=$3;
+        pesoMaximo=$4;
+        valorMinimo=$5;
+        valorMaximo=$6;
+        somaPesos=$7
+        arquivo="N"$n"-W"$w;
+        echo "$w" > $arquivo
+        echo "$n" >> $arquivo
+        t=0;
+        for (( i=1; i <= $n; i++))
+        do
+            pesoItem=$(( ( RANDOM % ($pesoMaximo-$pesoMinimo+1) )  + $pesoMinimo ))
+            valorItem=$(( ( RANDOM % ($valorMaximo-$valorMinimo+1) )  + $valorMinimo ))
+            t=$((pesoItem+t));
+            echo "$pesoItem $valorItem" >> $arquivo
+        done
+        if [ $t = $somaPesos ] 
+        then 
+            fim=1
+            mv $arquivo $arquivo"T"$t".txt"
+        else
+            rm $arquivo
+        fi
     done
-    mv $arquivo $arquivo"T"$t".txt"
 fi
